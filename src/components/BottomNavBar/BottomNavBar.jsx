e1bb237063e921d46f5d5b3d1770e08e9a3cf945
SelamB <selambeyene2@gmail.com>
2023-11-17 10:48:57 -0800

    updates sign up form

--- src/components/BottomNavBar/BottomNavBar.jsx
+++ src/components/BottomNavBar/BottomNavBar.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service';
+// import * as userService from '../../utilities/users-service';
 
 export default function BottomNavBar({ user, setUser }) {
     
-    function handleLogOut() {
-        userService.logOut();
+    // function handleLogOut() {
+    //     userService.logOut();
 
-        setUser(null);
-    }
+    //     setUser(null);
+    // }
     
     return (
-        <nav>
+        <footer>
             <Link to="/home">Home</Link>
             &nbsp; | &nbsp;
             <Link to="/communitydashboard">Community Dash</Link>
@@ -18,6 +18,6 @@ export default function BottomNavBar({ user, setUser }) {
             <Link to="/addactivity">Add Activity</Link>
             &nbsp; | &nbsp;
             <Link to="/myactivity">My Activity</Link>
-        </nav>
+        </footer>
     );
 }
