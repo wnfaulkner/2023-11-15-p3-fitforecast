8675d79424a9fd20258b514c0c44abb188fd57bd
lvlivingston <lvlivingston@gmail.com>
2023-11-17 16:18:52 -0600

    added fit forecast button

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export default function DashboardPage() {
     return (
         <div className="page-content">
             <h1>Community Dashboard Page</h1>
+            <Link to="/home">See my FITforecast</Link>
         </div>
     );
 }
