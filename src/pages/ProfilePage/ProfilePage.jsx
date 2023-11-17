dbb96d9bcd4bf98a26262b44543f925f4c5b9817
lvlivingston <lvlivingston@gmail.com>
2023-11-17 14:28:30 -0600

    added top navitation icons

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import * as userService from '../../utilities/users-service';
+
+export default function ProfilePage({ user, setUser }) {
+    
+    function handleLogOut() {
+        userService.logOut();
+
+        setUser(null);
+    }
+    
+    return (
+        <nav className="top-nav">
+            <span>Welcome, {user.name} </span>
+            &nbsp;&nbsp;
+            <Link to="" onClick={ handleLogOut } >Log Out</Link>
+        </nav>
+    );
+}
