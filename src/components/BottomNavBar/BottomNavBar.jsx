c46fcd92e50315e551937e70f62a7ce8e2f9e43c
lvlivingston <lvlivingston@gmail.com>
2023-11-17 14:15:22 -0600

    added icons to bottom nav bar

--- src/components/BottomNavBar/BottomNavBar.jsx
+++ src/components/BottomNavBar/BottomNavBar.jsx
@@ -1,4 +1,8 @@
 import { Link } from 'react-router-dom';
+import IconHome from '../../images/icons/IconHome/IconHome.png';
+import IconCommunity from '../../images/icons/IconCommunity/IconCommunity.png';
+import IconAdd from '../../images/icons/IconAdd/IconAdd.png';
+import IconMyActivity from '../../images/icons/IconMyActivity/IconMyActivity.png';
 // import * as userService from '../../utilities/users-service';
 
 export default function BottomNavBar({ user, setUser }) {
@@ -11,13 +15,13 @@ export default function BottomNavBar({ user, setUser }) {
     
     return (
         <footer className="bottom-nav">
-            <Link to="/home">Home</Link>
+            <Link to="/home"><img src={IconHome} alt="Home" /></Link>
             &nbsp; | &nbsp;
-            <Link to="/communitydashboard">Community Dash</Link>
+            <Link to="/communitydashboard"><img src={IconCommunity} alt="Community" /></Link>
             &nbsp; | &nbsp;
-            <Link to="/addactivity">Add Activity</Link>
+            <Link to="/addactivity"><img src={IconAdd} alt="Add Activity" /></Link>
             &nbsp; | &nbsp;
-            <Link to="/myactivity">My Activity</Link>
+            <Link to="/myactivity"><img src={IconMyActivity} alt="My Activity" /></Link>
         </footer>
     );
 }
