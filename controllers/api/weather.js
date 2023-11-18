e168545eaf0586bf3b9b3ebe4c8e2d7f56de578a
SelamB <selambeyene2@gmail.com>
2023-11-17 17:01:09 -0800

    just commiting so git lets me pull

--- controllers/api/weather.js
+++ controllers/api/weather.js
@@ -15,15 +15,17 @@ module.exports = {
 async function index(req, res, next) {
   //console.log("INDEX FUNCTION CALLED")
   try{
-    const endpointUrl = `${ROOT_URL}${endpoint}.json?key=${weatherApiKey}&q=83353&days=3&aqi=no&alerts=no`
+    const endpointUrl = `${ROOT_URL}${endpoint}.json?key=${weatherApiKey}&q=94608&days=3&aqi=no&alerts=no`
     const endpointData = await axios.get(
         endpointUrl, 
         { headers: { 'Content-Type': 'application/json' }}
       )
     //.then(res => res.json())
 
-    res.json(endpointData.data.forecast.forecastday)
-    console.log(endpointData.data.forecast.forecastday)
+    // res.json(endpointData.data.forecast.forecastday)
+    // console.log(endpointData.data.forecast.forecastday)
+    res.json(endpointData.data)
+    console.log(endpointData.data)
     //res.redirect('/')
   } catch (error) {
     console.error('Error fetching data from the API:', error);
