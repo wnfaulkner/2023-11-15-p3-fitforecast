7039f65cb306d407450cc462d3a0d541422a0f92
wnf <william@fluxrme.com>
2023-11-20 14:03:03 -0500

    adds todays average weather conditions to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,8 +6,13 @@ import { checkToken } from "../../utilities/users-service";
 export default function HomePage({ weatherData }) {
     
     const locationName = weatherData.location.name
+    const regionName = weatherData.location.region
+    const todayAvgTemp = weatherData.forecast.forecastday[0].day.avgtemp_f
+    const todayAvgConditionIcon = weatherData.forecast.forecastday[0].day.condition.icon
+    const todayAvgConditionText = weatherData.forecast.forecastday[0].day.condition.text
     
-    console.log(locationName);
+    console.log(weatherData)
+    console.log(todayAvgConditionIcon);
 
     async function handleCheckToken() {
         const expDate = await checkToken()
@@ -17,7 +22,10 @@ export default function HomePage({ weatherData }) {
     return (
         <div className="page-content">
             <h1>Today's Weather</h1>
-            <p>{locationName}</p>
+            <p>{locationName}, {regionName}</p>
+            <p>Today's Average Temp: {todayAvgTemp}&deg;F</p>
+            <img src={todayAvgConditionIcon} />
+            <p>{todayAvgConditionText}</p>
             <Link to="/addactivity">Log this Work-Out</Link>
         </div>
     );
