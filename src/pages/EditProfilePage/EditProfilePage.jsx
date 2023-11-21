aec043a632ddf1c9aad5a2306c16b348e354e3c3
nholliday314 <nholliday314@gmail.com>
2023-11-21 09:18:55 -0800

    edit profile feature

--- src/pages/EditProfilePage/EditProfilePage.jsx
+++ src/pages/EditProfilePage/EditProfilePage.jsx
@@ -1,7 +1,41 @@
-export default function EditProfilePage() {
+import React from "react";
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+import * as userService from '../../utilities/users-service';
+
+
+export default function EditProfilePage({ user, setUser }) {
+    const [formData, setFormData] = useState("");
+    function handleUpdate(evt) {
+        evt.preventDefault();
+        setUser({ location: formData.updatedUserLocation });
+    }
+
+    function handleLogOut() {
+        userService.logOut();
+        setUser(null);
+    }
+
     return (
         <div className="page-content">
-            <h1>Edit Profile Page</h1>
+            <form onSubmit={handleUpdate}>
+                <h1> Edit Profile Page</h1>
+                <p>{user.profilePic}</p>
+                <p>{user.username}</p>
+                <p>Current User Location: {user.location}</p>
+                <input name="updatedUserLocation"
+                    type="text" 
+                    id="textInput" 
+                    placeholder="Update Location"
+                    onChange={(evt) =>
+                    setFormData({
+                        ...formData,
+                        [evt.target.name]: evt.target.value,})}
+                    />
+                <button type="submit">Save</button>
+                
+                <Link to="" onClick={ handleLogOut } >Log Out</Link>
+            </form>
         </div>
     );
 }
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
 import * as userService from '../../utilities/users-service';
+import EditProfilePage from '../EditProfilePage/EditProfilePage';
 
 export default function ProfilePage({ user, setUser }) {
     const profilePic = user.profilePic;
     const username = user.name;
     const email = user.email;
     const location = user.location;
+    const [editMode, setEditMode] = useState(false);
+    const changeToFalse = () => {
+        setEditMode(false)
+
+    }
 
     function handleLogOut() {
         userService.logOut();
