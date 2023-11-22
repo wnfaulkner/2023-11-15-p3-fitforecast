2fb5da4f0f50ad4bebf5f4ea752dccaa09e68f90
lvlivingston <lvlivingston@gmail.com>
2023-11-22 13:46:41 -0600

    delete function working, need to fix rerender of myactivity page

--- src/pages/EditActivityPage/EditActivityPage.jsx
+++ src/pages/EditActivityPage/EditActivityPage.jsx
@@ -1,23 +1,24 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
-import { getAct, updateAct } from '../../utilities/activity-service';
-
+import { useNavigate } from 'react-router-dom';
+import { getAct, getActs, updateAct, deleteAct } from '../../utilities/activity-service';
 
 export default function EditActivityPage() {
-    const navigate = useNavigate();
-    const { activityId } = useParams();
-    const [currentActivity, setCurrentActivity] = useState(null)
-    console.log(`the current activity is: ${currentActivity}`)
-    console.log(`the current activityID is: ${activityId}`)
-    useEffect(() =>{
-        async function settingAct (){
-            const setAct = await getAct(activityId);
-            console.log('setAct:',setAct)
-            setCurrentActivity(setAct);
-        }
-        settingAct();
-    }, [activityId])
+  const navigate = useNavigate();
+  const { activityId } = useParams();
+  const [currentActivity, setCurrentActivity] = useState(null);
+  const [activities, setActivities] = useState(null); // Declare the state variable
+  console.log(`the current activity is: ${currentActivity}`);
+  console.log(`the current activityID is: ${activityId}`);
+  
+  useEffect(() => {
+    async function settingAct() {
+      const setAct = await getAct(activityId);
+      console.log('setAct:', setAct);
+      setCurrentActivity(setAct);
+    }
+    settingAct();
+    }, [activityId]);
     function handleInputChange (evt) {
         setCurrentActivity((oldActivity) => ({...oldActivity,[evt.target.name]:evt.target.value}));
     }
@@ -30,6 +31,17 @@ export default function EditActivityPage() {
             console.log('Error submitting new activity details', err);
         }
     }
+    async function handleDelete() {
+        try {
+          await deleteAct(activityId);
+          // Update the state or re-fetch the activities after deletion
+          const updatedActivities = await getActs(); // Assuming you have a getActs function
+          setActivities(updatedActivities); // Set the state variable
+          navigate('/myactivity');
+        } catch (err) {
+          console.error('Error deleting activity', err);
+        }
+      }
     if (!currentActivity) { return <h1>Loading....</h1>}
     return (
         <div className="page-content">
@@ -106,117 +118,7 @@ export default function EditActivityPage() {
                 /><br/>
                 <button type="submit" >Update Activity</button>
             </form>
-                <button type="" >Delete Activity</button>
+                <button type="button" onClick={handleDelete}>Delete Activity</button>
         </div>
     );
 }
-
-// export default function EditActivityPage() {
-//     const navigate = useNavigate();
-//     const { activityId } = useParams();
-//     const [activity, setActivity] = useState(null);
-//     console.log(`activity in edit page: ${activity}`)
-//     useEffect(() => {
-//         const getActivityDetails = async () => {
-//             try {
-//                 const response = await fetch(`/myactivity/${activityId}`)
-//                 const data = await response.json();
-//                 setActivity(data);
-//             } catch (err) {
-//                 console.log('Error getting activity details', err);
-//             }
-//         };
-//         getActivityDetails();
-//     }, [activityId])
-
-//     function handleInputChange (evt) {
-//         setActivity((oldActivity) => ({...oldActivity,[evt.target.name]:evt.target.value}));
-//     } 
-//     async function handleSubmit (evt) {
-//         evt.preventDefault();
-//         try {
-//             await fetch(`/myactivity/${activityId}`, {
-//                 method: 'PATCH',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                 }, body: JSON.stringify(activity)
-//             });
-//             await updateUserState();
-//             navigate('/myactivity')
-//         } catch (err) {
-//             console.log('Error submitting new activity details', err);
-//         }
-//     }
-//     return (
-//         <div className="page-content">
-//             <h1>Edit Activity Page</h1>
-//             <form >
-//             <input 
-//                     type="text" 
-//                     placeholder="Enter Activity Name"
-//                     name="name"
-//                     value={activity.name}
-//                     onChange={handleInputChange}
-//                     /><br/>
-//                 <label htmlFor="">Activity Type
-//                     <select 
-//                         name="activityType"
-//                         value={activity.activityType}
-//                         onChange={handleInputChange}
-//                         >
-//                         <option value="Run">Run</option>
-//                         <option value="Hike">Hike</option>
-//                         <option value="Yoga">Yoga</option>
-//                         <option value="Walk">Walk</option>
-//                         <option value="Weights">Weights</option>
-//                         <option value="Meditation">Meditation</option>
-//                     </select>
-//                 </label>
-//                 <input type="date" placeholder="Date" name="date"/><br/>
-//                 <label htmlFor="">Indoor/Outdoor
-//                     <select 
-//                         name="inOut"
-//                         value={activity.inOut}
-//                         onChange={handleInputChange}>
-//                         <option value="Indoor">Indoor</option>
-//                         <option value="Outdoor">Outdoor</option>
-//                     </select>
-//                 </label>
-//                 <label htmlFor="">Duration
-//                     <select 
-//                     name="duration"
-//                     value={activity.duration}
-//                     onChange={handleInputChange}
-//                     >
-//                         <option value="0-30mins">0-30mins</option>
-//                         <option value="30mins-1hr">30mins-1hr</option>
-//                         <option value="1hr-2hr">1hr-2hr</option>
-//                         <option value="2hr+">2hr+</option>
-//                     </select>
-//                 </label>
-//                 <label htmlFor="">Rating
-//                     <select 
-//                     name="rating"
-//                     value={activity.rating}
-//                     onChange={handleInputChange}
-//                     >
-//                         <option value="1">★</option>
-//                         <option value="2">★★</option>
-//                         <option value="3">★★★</option>
-//                         <option value="4">★★★★</option>
-//                         <option value="5">★★★★★</option>
-//                     </select>
-//                 </label>
-//                 <input 
-//                 type="text"
-//                 name="details"
-//                 placeholder="Description"
-//                 value={activity.description}
-//                 onChange={handleInputChange}
-//                 /><br/>
-//                 <button type="submit" onSubmit={handleSubmit}>Update Activity</button>
-//                 <button type="" >Delete Activity</button>
-//             </form>
-//         </div>
-//     );
-// }
