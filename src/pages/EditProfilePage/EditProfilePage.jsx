6efcefb52dd9a39716c6e7d21185f755fdf0e1a7
lvlivingston <lvlivingston@gmail.com>
2023-11-21 19:30:33 -0600

    ability for user to update location

--- src/pages/EditProfilePage/EditProfilePage.jsx
+++ src/pages/EditProfilePage/EditProfilePage.jsx
@@ -1,35 +1,58 @@
-import React from "react";
 import { useState } from "react";
-import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service';
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { updateUserState } from "../../utilities/users-service";
+
 export default function EditProfilePage({ user, setUser }) {
-    const [formData, setFormData] = useState("");
-    function handleUpdate(evt) {
+    const { userId } = useParams();
+    const profilePic = user.profilePic;
+    const username = user.name;
+    const email = user.email;
+    const location = user.location;
+    
+    const navigate = useNavigate();
+    const [updateProfile, setUpdateProfile] = useState({
+        profilePic: user.profilePic,
+        name: user.name,
+        email: user.email,
+        location: user.location,
+        user: user
+    });
+    
+    const handleLocationChange = (evt) => {
+        setUpdateProfile({...updateProfile, [evt.target.name]: evt.target.value})
+    };
+    async function handleSubmit(evt) {
         evt.preventDefault();
-        setUser({ location: formData.updatedUserLocation });
+        try {
+            await fetch('http://localhost:3001/api/users/profile/edit', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                }, body: JSON.stringify(updateProfile)
+            });
+            await updateUserState();
+            navigate('/profile')
+        }catch (error) {
+            console.error('Location Update Error', error)
+        }
     }
-    function handleLogOut() {
-        userService.logOut();
-        setUser(null);
-    }
+
     return (
         <div className="page-content">
-            <form onSubmit={handleUpdate}>
-                <h1> Edit Profile Page</h1>
-                <p>{user.profilePic}</p>
-                <p>{user.username}</p>
-                <p>Current User Location: {user.location}</p>
-                <input name="updatedUserLocation"
-                    type="text"
-                    id="textInput"
-                    placeholder="Update Location"
-                    onChange={(evt) =>
-                    setFormData({
-                        ...formData,
-                        [evt.target.name]: evt.target.value,})}
-                    />
-                <button type="submit">Save</button>
-                <Link to="" onClick={ handleLogOut } >Log Out</Link>
+            <form onSubmit={handleSubmit}>
+                <h1>Edit Profile Page</h1>
+                <p>{profilePic}</p>
+                <p>Username: {username}</p>
+                <p>Email: {email}</p>
+                <p>Current location: {location}</p>
+                <input 
+                    type="Number"
+                    placeholder="Type new zipcode here"
+                    name="location"
+                    onChange={handleLocationChange}
+                /><br/>
+                <button type="submit">Update Profile</button>
             </form>
         </div>
     );
